2ab38098910349c9a19a05281d4a2228167c46f2
Ncrite1 <zayatsn30@gmail.com>
2025-03-15 02:21:56 +0300

    fixx

--- profile.js
+++ profile.js
@@ -73,4 +73,4 @@ document.addEventListener("DOMContentLoaded", async function () {
         console.error("Ошибка при загрузке истории покупок:", error);
         purchaseList.innerHTML = "<p>Не удалось загрузить данные.</p>";
     }
-});
+});x
